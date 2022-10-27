15441b6ef7cd462fcac92881cf5ca12c7aa9aeae
locbq <buiquangloc1995@gmail.com>
2022-10-27 22:11:29 +0700

    setup firebase and github actions

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1yFqyvLc_vK7VtSbLi7uCBRNlPlVskEw",
+  authDomain: "fit-hanu-love.firebaseapp.com",
+  projectId: "fit-hanu-love",
+  storageBucket: "fit-hanu-love.appspot.com",
+  messagingSenderId: "306393000712",
+  appId: "1:306393000712:web:0aab772446c9afdca01abd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
